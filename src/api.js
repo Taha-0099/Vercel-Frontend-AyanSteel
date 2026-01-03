fa6cf397a256cc658fc01cf6919af678c0e246d5
Taha <tahaatif099@gmail.com>
2026-01-04 02:41:02 +0500

    Update API URL to Vercel backend

--- src/api.js
+++ src/api.js
@@ -1,9 +1,8 @@
 // src/api.js
 import axios from "axios";
 
-// CHANGE PORT IF YOUR BACKEND USES ANOTHER PORT
 const api = axios.create({
-  baseURL: "http://localhost:5001", // Node/Express backend
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5001",
 });
 
 export default api;
